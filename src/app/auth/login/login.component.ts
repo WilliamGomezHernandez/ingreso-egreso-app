ade275b8ff9877a2e9c72792f41d89a78e159412
William <william.gomez@ias.com.co>
2021-01-15 14:38:53 -0500

    Fin seccion 8

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as ui from '../../shared/ui.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   loginForm: FormGroup;
+  cargando = false;
+  uiSubscription: Subscription;
 
   constructor( private fb: FormBuilder,
                private authService: AuthService,
+               private store: Store<AppState> ,
                private router: Router) { }
 
   ngOnInit() {
@@ -22,27 +29,44 @@ export class LoginComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required ],
     });
+
+    this.uiSubscription = this.store.select('ui')
+      .subscribe( ui => {
+        this.cargando = ui.isLoading;
+        console.log('cargando subs');
+      } );
+  }
+
+  ngOnDestroy(){
+
+    this.uiSubscription.unsubscribe();
+
+
   }
 
   login(){
 
     if (this.loginForm.invalid) {return ; }
+
+    this.store.dispatch(ui.isLoading());
     //aqui
-    Swal.fire({
+/*     Swal.fire({
       title: 'Espere porfavor',
       didOpen: () => {
         Swal.showLoading();
       }
-    });
+    }); */
 
     const { email, password } = this.loginForm.value;
     this.authService.loginUsuario(email, password ).
       then( credenciales => {
         console.log(credenciales);
-        Swal.close();
+        //Swal.close();
+        this.store.dispatch(ui.stopLoading());
         this.router.navigate(['/']);
       })
       .catch( err => {
+        this.store.dispatch(ui.stopLoading());
         Swal.fire({
           icon: 'error',
           title: 'Oops...',
--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -2,6 +2,12 @@
 
 export class Usuario {
 
+    // tslint:disable-next-line:typedef
+    static fromFirebase({ email , uid, nombre }){
+
+        return new Usuario(uid, nombre, email);
+    }
+
     constructor(
         public uid: string,
         public nombre: string,
